578e1d8648efbc1632f2a80f3a948104677e2e9d
xushuo <893804934@qq.com>
2017-05-27 18:51:40 +0800

    响应式编程 可观察者，observable  观察者observer

---
+++ src/app/bind/bind.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {FormControl} from "@angular/forms";
+import 'rxjs/Rx'
+
+@Component({
+  selector: 'app-bind',
+  templateUrl: './bind.component.html',
+  styleUrls: ['./bind.component.css']
+})
+export class BindComponent implements OnInit {
+
+  searchInput:FormControl = new FormControl();
+
+  constructor() {
+    /*Observable.from([1,2,3,4])
+      .filter(e => e%2==0)
+      .map(e => e*e)
+      .subscribe(
+        e => console.log(e),
+        error => console.error(error),
+        () => console.log('结束了')
+      )*/
+    this.searchInput.valueChanges.debounceTime(500).subscribe((e)=>{
+      this.onkey(e);
+    })
+  }
+
+  ngOnInit() {
+  }
+  event1(event: any){
+    console.log(event);
+  }
+  onkey(val: String){
+    console.log(val);
+  }
+}
